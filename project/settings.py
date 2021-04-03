45c6d7ef42425b55c8874ccb4474613e4888d981
takashi3802 <shikataudone@yahoo.co.jp>
2021-04-03 17:01:50 +0900

    初期設定

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -118,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 管理サイトのログイン機能を通常のログイン機能として使う
+LOGIN_URL='admin:login'
+LOGOUT_REDIRECT_URL='/'
+
+# django-crispy-forms 設定
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
